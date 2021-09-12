f31d68456bdc1375045660e47d477abc54456198
amanda <amanda.blaze1988@gmail.com>
2021-09-11 19:40:33 -0700

    added create func and params to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,15 @@ class MoviesController < ApplicationController
         movies = Movie.all
         render json: movies
     end
+
+    def create 
+        movie = Movie.create(movie_params)
+        render json: movie
+    end
+
+    private 
+    
+    def movie_params 
+        params.require(:movie).permit(:title, :director, :release_date, :overview, :image_url)
+    end
 end
