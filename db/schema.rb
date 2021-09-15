635ff5156674a449fe6ac89ea84f8a73bb28396d
amanda <amanda.blaze1988@gmail.com>
2021-09-15 14:59:21 -0700

    Created Reviews and User models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_202215) do
+ActiveRecord::Schema.define(version: 2021_09_15_214937) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -22,4 +22,20 @@ ActiveRecord::Schema.define(version: 2021_09_08_202215) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "reviews", "movies"
 end
