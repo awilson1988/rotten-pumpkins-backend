d23272b803319198d7c7a6a2089f3f2f7c2eb484
amanda <amanda.blaze1988@gmail.com>
2021-09-24 16:19:07 -0700

    changes to routes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,4 +4,45 @@ class FavoritesController < ApplicationController
         favorite = Favorite.new
     end
 
+    def create
+        favorite = Favorite.new(favorite_params)
+        if favorite.save
+            render json: favorite, except: [:created_at, :updated_at]
+        else
+            render json: {message: "Favorite Unable to Ssve"}
+        end
+    end
+
+    def index
+        user_id = params[:user_id]
+        user = User.find(user_id)
+        favorites = user.favorites
+        render json: favorites, include: [:game]
+        # rendering related object data in JSON by nesting models
+        # result:
+          #       {
+          # "id": 2,
+          # "user_id": 1,
+          # "game": {
+          #   "id": 4,
+          #   "title": "",
+          #   "category": "",
+          #   "created_at": "2019-05-14T11:20:37.177Z",
+          #   "updated_at": "2019-05-14T11:20:37.177Z"
+          # }
+    end
+
+    def destroy
+        fav_id = params[:id]
+        favorite = Favorite.find(fav_id)
+        favorite.destroy
+    end
+
+
+private
+    def favorite_params
+      params.require(:favorite).permit(:user_id, :game_id)
+    end
 end
+
+
